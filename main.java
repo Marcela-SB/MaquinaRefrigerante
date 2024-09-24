0370c2df320a6fefcf19094fea915581b2dcec63
Marcela Silva <110864304+Marcela-SB@users.noreply.github.com>
2024-09-24 11:32:03 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,42 @@
-public class MaquinaRefrigerante{
-  public:
-	int preco;
-	int
+public class MaquinaRefrigerante {
+	
+    // Atributos.
+    private int preco;
+    private int balanco;
+    private int total;
+
+    // MÃ©todo construtor para inicializar os atributos
+    public MaquinaRefrigerante(int valor) {
+        preco = valor;
+        balanco = 0;
+        total = 0;
+    }
+
+    // Retorna o preÃ§o
+    public int getPreco(){
+        return preco;
+    }
+
+    // Retorna o balanÃ§o corrente
+    public int getBalanco(){
+        return balanco;
+    }
+
+    // Recebe um valor em dinheiro
+    public void inserirDinheiro(int valor){
+        balanco += valor;
+		System.out.println("Valor inserido com sucesso!!!!");
+    }
+
+    // Imprimir o preÃ§o do refrigerante
+    public void imprimirPreco(){
+        System.out.println("##################");
+        System.out.println("# Preco ##########");
+        System.out.println("# Refrigerante");
+        System.out.println("# R$ " + preco + ",00");
+        System.out.println("##################");
+    }
+		
 }
 
 
