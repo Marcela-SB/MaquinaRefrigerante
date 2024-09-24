069016fe7c03e697ff517cc1c63c33ec587afa51
Marcela Silva <110864304+Marcela-SB@users.noreply.github.com>
2024-09-24 11:17:38 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+public class MaquinaRefrigerante{
+  public:
+	int preco;
+	int
+}
+
+
+public class MaquinaRefrigeranteView{
+  public static void main(String args[]){
+    //
+    MaquinaRefrigerante maquina = new MaquinaRefrigerante(5);
+
+    maquina.imprimirPreco();
+    
+  }
+}
